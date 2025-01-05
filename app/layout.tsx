b0a8be45591c633a7de44fcee0f27bfb47b527d7
keshav-exe <keshav.bagaade@gmail.com>
2025-01-05 19:15:22 +0530

    v0.2 alpha

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { onest } from "@/lib/fonts";
-import Footer from "@/components/core-ui/footer";
 import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata = {
   title: "Create your own gradient wallpapers - GradientWalls",
@@ -66,9 +66,16 @@ export default function RootLayout({
       <body
         className={`${onest.variable} antialiased min-h-screen bg-background`}
       >
-        {children}
-        <Toaster />
-        <Footer />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster />
+          {/* <Footer /> */}
+        </ThemeProvider>
       </body>
     </html>
   );
